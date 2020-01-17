60bc6fffb478a18cae26f2ce9b5872ac6ff48686
Camilo <peronacamilo@gmail.com>
2020-01-17 00:41:44 -0300

    cadena

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const mysqlConnection = mysql.createConnection({
   host: 'magnetycs.com',
   user: 'magnetyc_cliente',
-  password: 'desarrollowedsab',
+  password: 'desarrolloweb',
   database: 'magnetycs_leads',
   multipleStatements: true
 });
