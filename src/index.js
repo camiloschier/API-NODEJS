5c4f89c70c808dc7c8af05937814889a548799bf
root <root@magnetycs.com>
2020-01-20 19:21:48 +0000

    aasd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 var cors = require('cors');
 const app = express();
-
+const https = require('https')
+const fs = require('fs');
 
 // Settings
 app.set('port', process.env.PORT || 3000);
@@ -14,6 +15,14 @@ app.use(express.json());
 app.use(require('./routes/routes'));
 
 // Starting the server
-app.listen(app.get('port'), () => {
-  console.log(`Server on port ${app.get('port')}`);
-});
+//app.listen(app.get('port'), () => {
+//  console.log(`Server on port ${app.get('port')}`);
+//});
+
+//SSL
+https.createServer({
+  key: fs.readFileSync('../key.pem'),
+  cert: fs.readFileSync('../private.pem'),
+  passphrase: 'sancorseguros'
+}, app)
+.listen(app.get('port'));
