628656e06081be09c80c3e9635f42b068073195d
Camilo <peronacamilo@gmail.com>
2020-01-22 20:05:18 -0300

    ssl desactivado

--- src/index.js
+++ src/index.js
@@ -15,15 +15,15 @@ app.use(express.json());
 // Routes
 app.use(require('./routes/routes'));
 
-// Starting the server
-//app.listen(app.get('port'), () => {
-//  console.log(`Server on port ${app.get('port')}`);
-//});
+//Starting the server
+  app.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`);
+});
 
 //SSL
-https.createServer({
-  key: fs.readFileSync(path.resolve('./src/key.pem')),
-  cert: fs.readFileSync(path.resolve('./src/cert.pem')),
-  passphrase: 'sancorseguros'
-}, app)
-.listen(app.get('port'));
+// https.createServer({
+//   key: fs.readFileSync(path.resolve('./src/key.pem')),
+//   cert: fs.readFileSync(path.resolve('./src/cert.pem')),
+//   passphrase: 'sancorseguros'
+// }, app)
+// .listen(app.get('port'));
