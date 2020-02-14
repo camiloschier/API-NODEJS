ce454fc582ef9212ebc2c146fc9905006ed5e81a
Camilo <peronacamilo@gmail.com>
2020-02-14 05:07:45 -0300

    s

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -23,7 +23,7 @@ router.post('/lead', function (req, res) {
   console.log("Nombre", persona.nombre);
   // res.send("Exito")
   mysqlConnection.query
-  ("INSERT INTO sancor_salud(nombre,apellido,edad,email,telefono,plan,tipo) VALUES ('"+persona.nombre+"','"+persona.apellido+"','"+persona.edad+"','"+persona.email+"','"+persona.telefono+"','"+persona.plan+"','"+persona.tipo+"')", (err, rows, fields) => {
+  ("INSERT INTO sancor_salud(nombre,apellido,edad,email,telefono,plan,tipo,keyword) VALUES ('"+persona.nombre+"','"+persona.apellido+"','"+persona.edad+"','"+persona.email+"','"+persona.telefono+"','"+persona.plan+"','"+persona.tipo+"','"+persona.keyword+"')", (err, rows, fields) => {
     console.log()
     if (!err) {
       res.json(rows);
