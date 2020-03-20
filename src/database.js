54ec457cfc1f89ce3a6f0bd9f7a419701b24ba88
Camilo <peronacamilo@gmail.com>
2020-03-19 21:30:03 -0300

    1

--- src/database.js
+++ src/database.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql');
 
 
+// const mysqlConnection = mysql.createConnection({
+//   host: 'magnetycs.com',
+//   user: 'magnetyc_cliente',
+//   password: 'desarrolloweb',
+//   database: 'magnetyc_leads',
+//   multipleStatements: true
+// });
+
 const mysqlConnection = mysql.createConnection({
-  host: 'magnetycs.com',
-  user: 'magnetyc_cliente',
+  host: 'http://apimagnetycs.ddns.net/',
+  user: 'admin',
   password: 'desarrolloweb',
-  database: 'magnetyc_leads',
+  database: 'leads',
   multipleStatements: true
 });
 
